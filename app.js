80c621d3ac80eaea12c7cd0a78c7ffbffae6cef4
Daouda Fofana <daouda.fofana@wezen.com>
2024-05-31 15:52:02 +0200

    update cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app
     .use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*')
         res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE')
-        res.header('Access-Control-Allow-Headers', 'Content-Type', 'Authorization')
+        res.header('Access-Control-Allow-Headers', '*')
         next()
     })
 
