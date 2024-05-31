4dfa6a6bc4bf16b9cea68ca55c88304104dec36a
Daouda Fofana <daouda.fofana@wezen.com>
2024-05-31 09:15:42 +0200

    add cors middleware

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app
     .use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Type");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Type, content-type");
         next();
 
     })
